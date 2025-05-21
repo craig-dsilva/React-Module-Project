a2cec154b65a0873f0a2c55085093d48ba8e6b56
Craig D'Silva <craig.s.dsilva@gmail.com>
2025-05-21 19:20:44 +0100

    Update fetch URL

--- src/components/Bookings/Bookings.jsx
+++ src/components/Bookings/Bookings.jsx
@@ -14,7 +14,7 @@ const Bookings = () => {
   useEffect(() => {
     const fetchBookings = async () => {
       try {
-        const res = await fetch(`http://52.208.38.55/bookings`);
+        const res = await fetch(`http://34.242.139.147:3001/bookings`);
         const data = await res.json();
         setBookings(data);
       } catch (error) {
